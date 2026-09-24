fb9c6c0810b2ddb93a28e6a4c2225f024de9397b
supportkarlodeal-creator <support.karlodeal@gmail.com>
2026-09-24 22:28:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
 import ProjectTabs from "@/components/ProjectTabs";
-import FeaturedProjects from "@/components/FeaturedProjects";
+//import FeaturedProjects from "@/components/FeaturedProjects";//
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import FloatingActions from "@/components/FloatingActions";
